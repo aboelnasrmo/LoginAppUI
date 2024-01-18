9c2d2459e91e038f5fa5906bf30845b72097daca
mohammadaboelnasr <m.aboelnasr@live.com>
2024-01-18 18:02:50 +0300

    added Login Screen Button and Text Field

--- LoginAppUI/ContentView.swift
+++ LoginAppUI/ContentView.swift
@@ -10,10 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .padding(.bottom, 50)
+            TextField("Email", text: .constant(""))
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(10)
+                .padding(.bottom, 20)
+            SecureField("Password", text: .constant(""))
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(10)
+                .padding(.bottom, 20)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 220, height: 60)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(15)
+            })
+            Spacer()
         }
         .padding()
     }
