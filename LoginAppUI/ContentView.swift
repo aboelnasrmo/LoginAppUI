b09e98b07f2282e7dd27d92d3d1fca34a0ee9ba1
mohammadaboelnasr <m.aboelnasr@live.com>
2024-01-18 18:01:13 +0300

    Master repo

---
+++ LoginAppUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginAppUI
+//
+//  Created by Mohammad Aboelnasr on 18/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginAppUI/LoginAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppUIApp.swift
+//  LoginAppUI
+//
+//  Created by Mohammad Aboelnasr on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
