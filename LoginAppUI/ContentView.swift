2717b5d764590bebfd6dd4a1507ba3c47e49f05e
mohammadaboelnasr <m.aboelnasr@live.com>
2024-01-18 18:21:24 +0300

    adding custom button

--- LoginAppUI/ContentView.swift
+++ LoginAppUI/ContentView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Circle()
-                .foregroundColor(.red.opacity(0.7))
-                .offset(y:-100)
-                .frame(width: 900,height: 900)
-            Circle()
-                .foregroundColor(.red.opacity(0.6))
-                .offset(y:-150)
-                .frame(width: 900,height: 900)
-            Circle()
-                .foregroundColor(.red.opacity(0.5))
-                .offset(y:-40)
-                .frame(width: 900,height: 900)
+//            Circle()
+//                .foregroundColor(.red.opacity(0.7))
+//                .offset(y:-100)
+//                .frame(width: 900,height: 900)
+//            Circle()
+//                .foregroundColor(.red.opacity(0.6))
+//                .offset(y:-150)
+//                .frame(width: 900,height: 900)
+//            Circle()
+//                .foregroundColor(.red.opacity(0.5))
+//                .offset(y:-40)
+//                .frame(width: 900,height: 900)
             VStack {
                 Image("logo")
                     .resizable()
@@ -34,26 +34,20 @@ struct ContentView: View {
                     .background(Color(.systemGray6))
                     .cornerRadius(10)
                     .padding(.bottom, 20)
-                SecureField("Password", text: .constant(""))
-                   
+                SecureField("Password", text: .constant("")
+                    
+                )
+                    .foregroundColor(.blue)
+                    
                     .padding()
                 
                     .background(Color(.systemGray6))
                    
                     .cornerRadius(10)
                     .padding(.bottom, 20)
-                Button(action: {}, label: {
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 220, height: 60)
-                        
-                        .background(.orange.opacity(0.9))
+                CustomButton(title: "Login") {
                     
-                        .cornerRadius(15)
-                    
-                })
+                }
                 Spacer()
             }
             .padding()
